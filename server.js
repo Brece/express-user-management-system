5ca0a0ac65ee932d5b229c66413a37a27ba86970
Nui Ruppert <44260650+Brece@users.noreply.github.com>
2022-09-09 18:28:41 +0200

    adds basic server settings

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const path = require('path');
+const morgan = require('morgan');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
+
+// log requests
+app.use(morgan('tiny'));
+
+// parse request to body
+app.use(express.urlencoded({ extended: false }));
+
+// set view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// load assets
+app.use('/img', express.static(path.join(__dirname, 'public/img')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
 
 app.get('/', (req, res) => {
     res.send('App running....');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT: ${PORT}...`);
+    console.log(`Server is running on http://localhost:${PORT}...`);
 });
